0e2228684e6108d18107c3f9a24df9f24e76cb59
wihl520 <1305501358@qq.com>
2019-11-13 18:11:15 +0800

    提交CPU爆满测试

--- src/main/java/com/wenas/hello/quartz/demo/job/SchedulerQuartzJob1.java
+++ src/main/java/com/wenas/hello/quartz/demo/job/SchedulerQuartzJob1.java
@@ -4,7 +4,11 @@ import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * <p>Description: </p>
@@ -20,13 +24,38 @@ public class SchedulerQuartzJob1 implements Job {
 
     @Override
     public void execute(JobExecutionContext arg0) throws JobExecutionException {
-        before();
-        long timeStart = System.currentTimeMillis();
-        // TODO 业务
-        System.out.println("工作 SchedulerQuartzJob1 业务，当前执行时间" + new Date().toString());
-        long timeEnd = timeStart - System.currentTimeMillis();
-        System.out.println("执行时间长度：" + timeEnd);
-        after();
+//        before();
+//        long timeStart = System.currentTimeMillis();
+//        // TODO 业务
+//        System.out.println("工作 SchedulerQuartzJob1 业务，当前执行时间" + new Date().toString());
+//        long timeEnd = timeStart - System.currentTimeMillis();
+//        System.out.println("执行时间长度：" + timeEnd);
+//        after();
+        String[] patternMatch = {"([\\w\\s]+)+([+\\-/*])+([\\w\\s]+)",
+                "([\\w\\s]+)+([+\\-/*])+([\\w\\s]+)+([+\\-/*])+([\\w\\s]+)"};
+        List<String> patternList = new ArrayList<String>();
+
+        patternList.add("Avg Volume Units product A + Volume Units product A");
+        patternList.add("Avg Volume Units /  Volume Units product A");
+        patternList.add("Avg retailer On Hand / Volume Units Plan / Store Count");
+        patternList.add("Avg Hand Volume Units Plan Store Count");
+        patternList.add("1 - Avg merchant Volume Units");
+        patternList.add("Total retailer shipment Count");
+
+        for (String s : patternList) {
+            for (int i = 0; i < patternMatch.length; i++) {
+                Pattern pattern = Pattern.compile(patternMatch[i]);
+
+                Matcher matcher = pattern.matcher(s);
+                System.out.println(s);
+                //CPU飙升根源
+                if (matcher.matches()) {
+
+                    System.out.println("Passed");
+                } else
+                    System.out.println("Failed;");
+            }
+        }
     }
 
     private void after(){
